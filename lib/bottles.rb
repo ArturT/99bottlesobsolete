414f6d64c2041a8877dcca45104ae0ab6e14880a
ArturT <artur.trzop@lunarlogic.io>
2016-07-21 19:04:45 +0200

    Pass 3rd test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,5 +1,18 @@
 class Bottles
   def verse(num_of_bottles)
-    "#{num_of_bottles} bottles of beer on the wall, #{num_of_bottles} bottles of beer.\nTake one down and pass it around, #{num_of_bottles-1} bottles of beer on the wall.\n"
+    left_bottles = num_of_bottles - 1
+    pluralize_bottles = pluralize(left_bottles, 'bottle', 'bottles')
+
+    "#{num_of_bottles} bottles of beer on the wall, #{num_of_bottles} bottles of beer.\nTake one down and pass it around, #{left_bottles} #{pluralize_bottles} of beer on the wall.\n"
+  end
+
+  private
+
+  def pluralize(count, singular_word, plural_word)
+    if count == 1
+      singular_word
+    else
+      plural_word
+    end
   end
 end
