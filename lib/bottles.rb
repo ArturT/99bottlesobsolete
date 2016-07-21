f583cfbb82c6554a6197895a2f888d1aa0f6af72
ArturT <artur.trzop@lunarlogic.io>
2016-07-21 19:19:49 +0200

    5th test pass

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,9 +2,14 @@ class Bottles
   def verse(num_of_bottles)
     num_of_left_bottles = num_of_bottles - 1
 
-    first_sentense = "#{num_of_bottles} #{pluralize_bottles(num_of_bottles)} of beer on the wall, #{num_of_bottles} #{pluralize_bottles(num_of_bottles)} of beer."
+    if num_of_bottles.zero?
+      first_sentense = "No more bottles of beer on the wall, no more bottles of beer."
+      second_sentense = "Go to the store and buy some more, 99 bottles of beer on the wall."
+    else
+      first_sentense = "#{num_of_bottles} #{pluralize_bottles(num_of_bottles)} of beer on the wall, #{num_of_bottles} #{pluralize_bottles(num_of_bottles)} of beer."
 
-    second_sentense = "Take #{prefix_down(num_of_left_bottles)} down and pass it around, #{left_bottles_words(num_of_left_bottles)} of beer on the wall."
+      second_sentense = "Take #{prefix_down(num_of_left_bottles)} down and pass it around, #{left_bottles_words(num_of_left_bottles)} of beer on the wall."
+    end
 
     "#{first_sentense}\n#{second_sentense}\n"
   end
