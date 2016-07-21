3e33c34535a0c52f0c20de5cb16cbbcde988bac4
ArturT <artur.trzop@lunarlogic.io>
2016-07-21 18:59:13 +0200

    Pass the second test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,5 +1,5 @@
 class Bottles
-  def verse(_)
-    "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
+  def verse(num_of_bottles)
+    "#{num_of_bottles} bottles of beer on the wall, #{num_of_bottles} bottles of beer.\nTake one down and pass it around, #{num_of_bottles-1} bottles of beer on the wall.\n"
   end
 end
